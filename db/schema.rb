40751054fd16fd0909d25dd2d375e7641ce88731
Jennifer Chaiken <jenniferchaiken@yahoo.com>
2017-02-16 20:43:43 +0000

    add User model, controller, and tests

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170214035037) do
+ActiveRecord::Schema.define(version: 20170216173000) do
 
   create_table "accounts", force: :cascade do |t|
     t.string   "name"
@@ -59,5 +59,15 @@ ActiveRecord::Schema.define(version: 20170214035037) do
   end
 
   add_index "related_accounts", ["account_id"], name: "index_related_accounts_on_account_id"
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
