3c11aecca3973dbba95bcbda8aa2f71919e10ca4
Jennifer Chaiken <jenniferchaiken@yahoo.com>
2017-02-18 15:53:40 +0000

    fix migration

--- db/migrate/20170209171911_remove_cash_account_ref_from_transactions.rb
+++
@@ -1,8 +0,0 @@
-class RemoveCashAccountRefFromTransactions < ActiveRecord::Migration
-  def change
-    remove_foreign_key :transactions, column: :cash_account_id
-    remove_foreign_key :transactions, column: :bank_account_id
-    remove_foreign_key :transactions, column: :bill_id
-    remove_foreign_key :transactions, column: :credit_card_id
-  end
-end
