f15bc1e909cec80e49f20efe13780e474ffc7f60
Jennifer Chaiken <jenniferchaiken@yahoo.com>
2017-03-06 05:35:24 +0000

    little bits

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,36 @@
 class Category < ActiveRecord::Base
   has_many :notes
   validates :name, presence: true, uniqueness: true
+  
+  def this_months_totals
+    # iterate through each category
+    self.each do |c|
+      # iterate through each transaction in each category
+      c.note.each do |t|
+        # add each debit transaction to the total
+        if t.transactionn_type == "Debit"
+          category_total += t.amount
+        end
+      end
+    end
+    
+    
+    
+   
+    
+  end
+  
+  def last_90_days
+    
+  end
+  
+  def total_this_year
+    self.notes.each do |t|
+      if t.transaction_type == "Debit"
+        @total += t.amount
+      end
+    end
+  end
+  
+  
 end
