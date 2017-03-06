866752cdd72b84489bfd3f533d3c5e1c6a2f9381
Jennifer Chaiken <jenniferchaiken@yahoo.com>
2017-03-06 05:37:41 +0000

    fix bad idea

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,17 +2,17 @@ class Category < ActiveRecord::Base
   has_many :notes
   validates :name, presence: true, uniqueness: true
   
-  def this_months_totals
-    # iterate through each category
-    self.each do |c|
-      # iterate through each transaction in each category
-      c.note.each do |t|
-        # add each debit transaction to the total
-        if t.transactionn_type == "Debit"
-          category_total += t.amount
-        end
-      end
-    end
+  # def this_months_totals
+  #   # iterate through each category
+  #   self.each do |c|
+  #     # iterate through each transaction in each category
+  #     c.note.each do |t|
+  #       # add each debit transaction to the total
+  #       if t.transactionn_type == "Debit"
+  #         category_total += t.amount
+  #       end
+  #     end
+  #   end
     
     
     
@@ -24,13 +24,13 @@ class Category < ActiveRecord::Base
     
   end
   
-  def total_this_year
-    self.notes.each do |t|
-      if t.transaction_type == "Debit"
-        @total += t.amount
-      end
-    end
-  end
+  # def total_this_year
+  #   self.notes.each do |t|
+  #     if t.transaction_type == "Debit"
+  #       @total += t.amount
+  #     end
+  #   end
+  # end
   
   
 end
