be79773797712aad2bf557cce25f44d68f0d33f9
Jennifer Chaiken <jenniferchaiken@yahoo.com>
2017-11-01 15:26:15 +0000

    oooops...fixed home page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,8 +10,8 @@ class ApplicationController < ActionController::Base
  
   def require_login
     unless logged_in?
-      flash[:error] = "You must be logged in to access this section"
-      redirect_to root_path # halts request cycle
+      # flash[:error] = "You must be logged in to access this section"
+      redirect_to login_path
     end
   end
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,5 @@
 class PagesController < ApplicationController
+  before_action :require_login
   before_action :set_accounts
   
   def home
